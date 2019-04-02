7e5d49ae56a0b77b8efb9f54d08e07ace816fdca
RickN123 <nicolai.richard@gmail.com>
2019-04-02 18:11:41 -0400

    fix division

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,19 @@
 
 window.onload = function () {
     var Random = [Math.floor(Math.random() * 101 + 19);
-    $("#redcrystal").on("click", random);
-    $("#bluecrystal").on("click", random);
-    $("#yellowcrystal").on("click", random);
-    $("#greencrystal").on("click", random);
+
 };
 
+$("#red").onclick(.text(Random);
+$("#blue").onclick(.text(Random);
+$("#yellow").onclick(.text(Random);
+$("#green").onclick(.text(Random);
+};
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 0;
-var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var number = 
 var guessesDone = 0;
 var correctGuess = null;
 
@@ -28,10 +30,8 @@ function initialize() {
 
 
 document.onclick = function (event) {
-    if (guessesDone >= 10) {
-        return;
-    }
-    var random = event.key;
+    for (var i = 0; i < 1; i++)
+        var random = event.key;
     var computerGuess = alphabets[Math.floor(Math.random() * alphabets.length)];
 
 
